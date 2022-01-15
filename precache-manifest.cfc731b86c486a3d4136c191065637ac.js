6de85d5ac34e125e48c2e670481b40ba3a62cecb
Winson Cheng <w.winsoncheng@gmail.com>
2022-01-15 17:18:12 +0800

    Updates

---
+++ precache-manifest.cfc731b86c486a3d4136c191065637ac.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "7f1e7a51a4797ac9d160",
+    "url": "/blockchain-developer-bootcamp-final-project/static/css/main.7994f592.chunk.css"
+  },
+  {
+    "revision": "7f1e7a51a4797ac9d160",
+    "url": "/blockchain-developer-bootcamp-final-project/static/js/main.7f1e7a51.chunk.js"
+  },
+  {
+    "revision": "b5e4cf39d4d19f1e6f06",
+    "url": "/blockchain-developer-bootcamp-final-project/static/css/1.c99864b3.chunk.css"
+  },
+  {
+    "revision": "b5e4cf39d4d19f1e6f06",
+    "url": "/blockchain-developer-bootcamp-final-project/static/js/1.b5e4cf39.chunk.js"
+  },
+  {
+    "revision": "2242dfc52fd5369ed931",
+    "url": "/blockchain-developer-bootcamp-final-project/static/js/runtime~main.2242dfc5.js"
+  },
+  {
+    "revision": "2c1d862b192eec296fcb2a3fd7fe820b",
+    "url": "/blockchain-developer-bootcamp-final-project/static/media/bank.2c1d862b.png"
+  },
+  {
+    "revision": "da70255120845f3066c5bdcc7367f259",
+    "url": "/blockchain-developer-bootcamp-final-project/static/media/tether.da702551.png"
+  },
+  {
+    "revision": "c19b9563b0fc959bcc09ffe14052c2a6",
+    "url": "/blockchain-developer-bootcamp-final-project/index.html"
+  }
+];
